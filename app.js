00df74a7d5ba7affc338e8b60da74c86749ff3ea
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-08-05 10:19:16 +1200

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Tell express to use ejs template engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+var messages = [];
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+  console.log('post');
+  messages.push({
+    name: req.body.name,
+    message: req.body.message
+  });
+  io.emit('message', req.body);
+  console.log(messages);
+  res.status(200).send(messages);
+})
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+var server = http.listen(5000, () => {
+  console.log('server is running on port', server.address().port);
+});
